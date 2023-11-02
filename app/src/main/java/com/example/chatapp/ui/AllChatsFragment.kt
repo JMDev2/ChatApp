b04bfb1147914c9ddb157a3f5737fdac58e98bf1
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2023-11-02 20:30:47 +0300

    feat(display_chats): add the login and  display chats functionality

--- app/src/main/java/com/example/chatapp/ui/AllChatsFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/AllChatsFragment.kt
@@ -34,15 +34,7 @@ class AllChatsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
         binding = FragmentAllChatsBinding.inflate(inflater, container, false)
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
-        // Enable the back arrow in the toolbar
-        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        // Set the click listener for the back arrow
-        binding.toolbar.setNavigationOnClickListener {
-            NavHostFragment.findNavController(this).popBackStack()
-        }
         return binding.root
     }
 
